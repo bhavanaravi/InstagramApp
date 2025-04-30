a02be2a3913402110a2c18974da5a4803a27ce1c
bhavana <bhavana@bhavanas-MacBook-Air-4.local>
2025-04-30 14:54:25 +0530

    firebase sdk installed

---
+++ Instagram/Instagram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by bhavana  on 30/04/25.
+//
+
+import SwiftUI
+import Firebase
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
